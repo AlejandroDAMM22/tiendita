241f1dad58d67adb49c9f15e72b41586e1d64a5c
Alejandro Morales <116979446+AlejandroDAMM22@users.noreply.github.com>
2023-05-11 10:41:32 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -12,7 +12,6 @@ self.addEventListener('install', event=>{
             const cacheInstallEstatico = caches.open(cache_estatico) //open crea y abre el cache
               .then(cache=>{
                     return cache.addAll([
-                        '/',
                         'index.html'
 //                         'pages/Offline.html',
 //                         'pages/about.html',
