2d81d07a8ea0229d182a310f2d7c73e8c9fdbde7
Alejandro Morales <116979446+AlejandroDAMM22@users.noreply.github.com>
2023-05-10 22:16:35 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,43 +1,9 @@
-
-
-let SUCCESS_HANDLERS = [];
-let ERROR_HANDLERS = [];
-
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('{{ROOT_URL}}{{SERVICE_WORKER_FILENAME}}', { scope: '{{ROOT_URL}}' })
-    .then(function(reg) {
-      let current = Promise.resolve();
-
-      for (let i = 0, len = SUCCESS_HANDLERS.length; i < len; i++) {
-        current = current.then(function() {
-          return SUCCESS_HANDLERS[i](reg);
-        });
-      }
-
-      return current
-        .then(function() {
-          console.log('Service Worker registration succeeded. Scope is ' + reg.scope);
-        });
+  navigator.serviceWorker.register('/sw.js')
+    .then(function(registration) {
+      console.log('Service Worker registered with scope:', registration.scope);
     })
     .catch(function(error) {
-      let current = Promise.resolve();
-
-      for (let i = 0, len = ERROR_HANDLERS.length; i < len; i++) {
-        current = current.then(function() {
-          return ERROR_HANDLERS[i](error);
-        });
-      }
-
-      return current
-        .then(function() {
-          console.log('Service Worker registration failed with ' + error);
-        });
+      console.log('Service Worker registration failed:', error);
     });
 }
-
-
-
-
-
-// }
-
